eb132e51f837618a3cac2f80ff6697499b21fe60
charlie_wutong <2459060612@qq.com>
2019-08-25 16:48:43 +0800

    commit init

--- com/huawei/InjectJsExtension/js/content_script.js
+++ com/huawei/InjectJsExtension/js/content_script.js
@@ -1,3 +1,5 @@
+var contentText = '';
+
 function injectJs(jsPath) {
     var inject = document.createElement('script');
     inject.setAttribute('type', 'text/javascript');
@@ -12,20 +14,59 @@ function injectJs(jsPath) {
 // 向dom 注入js
 document.addEventListener('DOMContentLoaded', function () {
     console.info('content_script.js 我开始执行了！');
-    injectJs("js/3rd/jquery.min.js");
+
+    var scripts = document.scripts;
+    var isContainJquery = false;
+    if (scripts) {
+        var len = scripts.length;
+        if (len > 0) {
+            for (let i = 0; i < len; i++) {
+                var src1 = scripts[i].src;
+                if (src1.indexOf("jquery") != -1) {
+                    isContainJquery = true;
+                    break;
+                }
+            }
+        }
+    }
+    if (!isContainJquery) {
+        injectJs("js/3rd/jquery.min.js");
+    }
     injectJs("js/inject.js");
+
+    if (window.location.href == 'https://support.huawei.com/carrierindex/zh/hwe/index.html') {
+        document.getElementById("PBI1-7899438").setAttribute("class", "btn btn-danger");
+        document.getElementById("PBI1-7275733").setAttribute("class", "btn btn-success");
+
+        document.getElementById("PBI1-7899438").onclick = function () {
+            if (contentText == '') {
+                contentText = this.text;
+                this.text = "content-script js 事件绑定";
+            } else {
+                this.text = contentText;
+                contentText = '';
+            }
+            console.log('content-script访问dom中js数据:' + path);
+        }
+
+    }
     console.info('content_script.js 我执行完毕了！');
 });
+// 1.当 onload 事件触发时，页面上所有的DOM，样式表，脚本，图片，flash都已经加载完成了。
+
+// 2.当 DOMContentLoaded 事件触发时，仅当DOM加载完成，不包括样式表，图片，flash。
+
 
 // content-script 接收到 popup/background 的信息
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.cmd != undefined && request.cmd == 'speek') {
-        //let msg = new SpeechSynthesisUtterance(request.msg);
-        //speechSynthesis.speak(msg);
+        let msg = new SpeechSynthesisUtterance(request.msg);
+        speechSynthesis.speak(msg);
     }
-
     sendResponse('我收到了你的消息！');
 });
+
 
 // content_scripts向popup主动发消息的前提是popup必须打开！否则需要利用background作中转；
 // 如果background和popup同时监听，那么它们都可以同时收到消息，但是只有一个可以sendResponse，一个先发送了，那么另外一个再发送就无效
@@ -36,14 +77,19 @@ window.addEventListener("message", function (e) {
     }
 }, false);
 
+//  content-script 向 popup/background 发送数据
 
 setInterval(function () {
-    //  content-script 向 popup/background 发送数据
+
     chrome.runtime.sendMessage({"send": "content-script", "cmd": "log", "msg": 'xxx'}, function (response) {
         console.log('收到来自后台的回复：' + response);
     });
-    //
-    window.postMessage({"send": "content-srcipt", "cmd": "content_log", "msg": "我是content-script,发送数据给inject脚本"}, "*");
+
+    window.postMessage({
+        "send": "content-srcipt",
+        "cmd": "content_log",
+        "msg": "我是content-script,发送数据给inject脚本"
+    }, "*");
 
 }, 10000);
 
--- com/huawei/InjectJsExtension/js/inject.js
+++ com/huawei/InjectJsExtension/js/inject.js
@@ -7,3 +7,19 @@ window.addEventListener("message", function (e) {
 setInterval(function () {
     window.postMessage({"send": "inject", "cmd": "inject_log", "msg": "我是inject脚本,发送数据给content-script"}, '*');
 }, 2000);
+var beforeText = '';
+if (window.location.href == 'https://support.huawei.com/carrierindex/zh/hwe/index.html') {
+    $("#PBI1-7899438").attr("href", "javascript:;");
+    $("#PBI1-7275733").attr("href", "javascript:;");
+
+    $("#PBI1-7275733").bind('click', function () {
+        if (beforeText == '') {
+            beforeText = $(this).text();
+            $(this).text("inject js 事件绑定");
+        } else {
+            $(this).text(beforeText);
+            beforeText = '';
+        }
+        console.log('inject访问dom中js数据:' + path);
+    });
+}
